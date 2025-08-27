91314c35da3d618e805ee884ada0c94049d7b6e9
grishmaaa <grishma.renuka@gmail.com>
2025-08-27 20:05:47 +0530

    test

--- z_learning_lib/test/test.py
+++ z_learning_lib/test/test.py
@@ -1,2 +1,32 @@
+import random, time
 import fastmatmul
-print(fastmatmul.matmul([[1,2],[3,4]], [[5,6],[7,8]]))  # -> [[19.0, 22.0], [43.0, 50.0]]
+# import fastmatmul1 as fastmatmul
+import numpy as np
+import random, time
+
+def make_matrix(n):
+    return [[random.random() for _ in range(n)] for _ in range(n)]
+
+sizes = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]
+
+# header
+print(f"{'Size':>10} | {'Naive fastmatmul1 (s)':>25} | {'NumPy (OpenBLAS) (s)':>25}")
+print("-" * 65)
+
+for n in sizes:
+    # naive C extension
+    A = make_matrix(n)
+    B = make_matrix(n)
+    t0 = time.time()
+    fastmatmul.matmul(A, B)
+    naive_time = time.time() - t0
+
+    # numpy/OpenBLAS
+    A_np = np.random.rand(n, n)
+    B_np = np.random.rand(n, n)
+    t0 = time.time()
+    A_np @ B_np
+    numpy_time = time.time() - t0
+
+    # row
+    print(f"{n:>10} | {naive_time:>25.6f} | {numpy_time:>25.6f}")
